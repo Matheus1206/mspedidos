9d10a69c8922619d49a4c3e300ea28a7c062c2f6
Matheus1206 <bandeiramatheus@hotmail.com>
2024-05-01 16:23:58 -0300

    fix: arrumando o retorno do json do pedido, adicionando o pedido_id

--- gestao-pedidos/src/main/java/br/com/fiap/gestaopedidos/dto/PedidoResponse.java
+++ gestao-pedidos/src/main/java/br/com/fiap/gestaopedidos/dto/PedidoResponse.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 
-public record PedidoResponse(String id, @JsonProperty("cliente_id") String idCliente, List<Itens> itens){
+public record PedidoResponse(@JsonProperty("pedido_id") String id, @JsonProperty("cliente_id") String idCliente, List<Itens> itens){
     public PedidoResponse(Pedido pedido) {
         this(pedido.getId(), pedido.getIdCliente(),pedido.getItens());
     }
